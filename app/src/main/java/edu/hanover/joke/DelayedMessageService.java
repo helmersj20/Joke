2c66f448b5fd4865e5756c43b09fe4bde3160a5e
Jonathan Helmers <helmersj20@hanover.edu>
2018-05-17 11:50:32 -0400

    Initial commit

---
+++ app/src/main/java/edu/hanover/joke/DelayedMessageService.java
@@ -0,0 +1,13 @@
+package edu.hanover.joke;
+
+import android.app.IntentService;
+import android.content.Intent;
+public class DelayedMessageService extends IntentService {
+    public DelayedMessageService() {
+        super("DelayedMessageService");
+    }
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        //Code to do something
+    }
+}
